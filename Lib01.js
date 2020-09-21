de41c6f6bab1fc4e8c2ec4d466983664fdbf1adf
Adriana Kelly <adrianakelly.aise@gmail.com>
2020-09-21 04:09:35 -0400

    Committing first library

---
+++ Lib01.js
@@ -0,0 +1,35 @@
+export function LibBasicCalculator()
+{
+    return("test");
+}
+
+/*{
+    return("test");
+}/*
+
+/*export function LibBasicCalculator(x = 0, y = 0, op)
+{
+    if (op == "+")
+    {
+        return X + y;
+    }
+    if (op == "-")
+    {
+        return X - y;
+    }
+    if (op == "*")
+    {
+        return X - y;
+    }
+    if (op == "/")
+    {
+            if (y == 0)
+                {
+                    return "Error: division by zero.";
+                }
+            else 
+                {
+                    return x / y;
+                }
+    }
+}*/
